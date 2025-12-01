811f8246863d6f3b51c2b0cf077f223f00064a5c
pKm720 <priyanshumahato0210@gmail.com>
2025-12-01 15:02:12 +0530

    Added king and queen moves

--- src/arbiter/arbiter.jsx
+++ src/arbiter/arbiter.jsx
@@ -1,13 +1,17 @@
-import {getBishopMoves, getKnightmoves, getRookMoves} from "./getMoves"
+import {getBishopMoves, getKingMoves, getKnightmoves, getQueenMoves, getRookMoves} from "./getMoves"
 
 const aribiter = {
     getRegularMoves : function({position,rank,file,piece}){
+        if(piece.endsWith("k"))
+            return getKingMoves({position,rank,file,piece})
         if(piece.endsWith("r"))
             return getRookMoves({position,rank,file,piece})
         if(piece.endsWith("b"))
             return getBishopMoves({position,rank,file,piece})
         if(piece.endsWith("n"))
             return getKnightmoves({position,rank,file})
+        if(piece.endsWith("q"))
+            return getQueenMoves({position,rank,file,piece})
     }
 }
 
--- src/arbiter/getMoves.jsx
+++ src/arbiter/getMoves.jsx
@@ -80,3 +80,60 @@ export const getBishopMoves = ({position,rank,file,piece}) => {
     console.log(moves);
     return moves;
 }
+export const getQueenMoves = ({position,rank,file,piece}) => {
+        const moves = [];
+        const us = piece[0];
+        const enemy = us === "w" ? "b" : "w";
+
+        const dirs = [
+            [1,1],
+            [1,-1],
+            [-1,-1],
+            [-1,1],
+            [-1,0],
+            [1,0],
+            [0,-1],
+            [0,1],
+        ]
+        dirs.forEach(dir=>{
+            for (let i = 1; i < 8; i++) {
+                const x = rank + (i*dir[0]);
+                const y = file + (i*dir[1]);
+                if(position?.[x]?.[y] === undefined){
+                    break
+                }
+                if(position[x][y].startsWith(enemy)){
+                    moves.push([x,y])
+                    break
+                }
+                if(position[x][y].startsWith(us)){
+                    break
+                }
+                moves.push([x,y])
+            }
+        })
+    return moves;
+}
+export const getKingMoves = ({position,rank,file,piece}) => {
+        const moves = [];
+        const us = piece[0];
+        const enemy = us === "w" ? "b" : "w";
+
+        const dirs = [
+            [0,1],
+            [1,1],
+            [1,0],
+            [1,-1],
+            [0,-1],
+            [-1,-1],
+            [-1,0],
+            [-1,1]
+        ]
+        dirs.forEach(dir=>{
+                const x = rank + (dir[0]);
+                const y = file + (dir[1]);
+                if(position?.[x]?.[y] !== undefined && !position[x][y].startsWith(us))
+                    moves.push([x,y])
+        })
+    return moves;
+}
